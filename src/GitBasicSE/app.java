c9c31d4a6a8aeb1b3a5fdfbfa48d516d28e8caa5
maladaleewalan <malada.l@ku.th>
2020-07-23 09:47:55 +0700

    bring back App.java from commit 103019e

--- src/GitBasicSE/app.java
+++ src/GitBasicSE/app.java
@@ -2,6 +2,6 @@ package GitBasicSE;
 
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello Animal!");
+        System.out.println("Hello World!");
     }
 }
