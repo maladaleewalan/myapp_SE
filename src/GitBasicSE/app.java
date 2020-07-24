5caf4588d3c8a1494c0f372e3e0986fe3db0ec92
maladaleewalan <malada.l@ku.th>
2020-07-24 20:49:31 +0700

    update App.java by feature2

--- src/GitBasicSE/app.java
+++ src/GitBasicSE/app.java
@@ -2,6 +2,6 @@ package GitBasicSE;
 
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Feature2!");
     }
 }
